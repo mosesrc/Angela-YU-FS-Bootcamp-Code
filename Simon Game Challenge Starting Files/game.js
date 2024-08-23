e10b2a7db651ee35477c06a133e8d2a1bb002fc1
Ryan Moses <moses_ryan@bah.com>
2024-08-23 14:39:12 -0400

    Final Simon game files

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,14 +1,29 @@
 var userClickedPattern = [];
 var gamePattern = [];
 var buttonColours = ['red', 'blue', 'green', 'yellow'];
+var level = 0;
+var started = false;
 
 $('div[type=button]').click(function () {
     var userChosenColour = $(this).attr('id');
+    $(`#${userChosenColour}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(userChosenColour);
     userClickedPattern.push(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+$(document).keypress(function () {
+    if (!started) {
+        $('h1').text(`Level ${level}`);
+        nextSequence();
+        started = true;
+    }
 });
 
 function nextSequence() {
+    level++;
+    $('h1').text(`Level ${level}`);
+    
     var randNum = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randNum];
 
@@ -28,4 +43,35 @@ function animatePress(currentColour) {
     setTimeout(() => {
         $(`#${currentColour}`).removeClass('pressed');
     }, 100)
+}
+
+function checkAnswer(currentLevel) {
+    if (userClickedPattern[currentLevel] === gamePattern[gamePattern.length - 1]) {
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+
+    } else {
+        console.log('wrong');
+        var audio = new Audio('sounds/wrong.mp3');
+        audio.play();
+
+        $('body').addClass('game-over');
+
+        setTimeout(() => {
+            $('body').removeClass('game-over');
+        }, 200)
+
+        $('h1').text('Game Over, Press Any Key to Restart');
+        startOver();
+    }
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    userClickedPattern = []
+    started = false;
 }
